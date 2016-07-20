04100a5145446847dd2ea965feb0a77707727b48
sinhoisinh01 <doanphucsinh@gmail.com>
2016-07-20 10:41:16 +0700

    Add table descriptions

---
+++ database/migrations/2016_07_20_101201_descriptions.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Descriptions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('descriptions', function(Blueprint $table) {
+            $table->increments('id');
+			$table->integer('category_id')->unsigned();
+            $table->string('work_id');
+            $table->string('content');
+			$table->float('amount')->unsigned();
+			$table->float('length')->unsigned();
+			$table->float('width')->unsigned();
+			$table->float('height')->unsigned();
+            $table->foreign(['category_id', 'work_id'])->references(['category_id', 'work_id'])->on("category_work");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('descriptions');
+    }
+}
