4f0ee22abbb962da98f9016a909eeb506b3c7824
sinhoisinh01 <doanphucsinh@gmail.com>
2016-07-27 14:56:04 +0700

    Clean addConstruction modal with AngularJS and implement New, Open button in menu

--- public/js/workplace/menuController.js
+++ public/js/workplace/menuController.js
@@ -1,7 +1,23 @@
 angular.module('HMS')
-    .controller('MenuController', function ($rootScope, $stateParams, $scope) {
+    .controller('MenuController', function ($rootScope, $stateParams, $scope, $uibModal) {
         $rootScope.constructionName = $stateParams.name;
         $scope.collapseCategories = function() {
             $rootScope.isCollapsedCategories = !$rootScope.isCollapsedCategories;
+        };
+		$scope.add = function () {
+            $uibModal.open({
+                templateUrl: 'views/modals/addConstruction.html',
+                controller: 'AddConstructionController'
+            }).result.then(function (construction) {
+                $state.go('construction', {'construction_id': construction.id, name:construction.name });
+            });
+        };
+		$scope.viewAll = function () {
+            $uibModal.open({
+                templateUrl: 'views/modals/allConstructions.html',
+                controller: 'AllConstructionsController'
+            }).result.then(function (construction_id) {
+                $state.go('construction', {'construction_id': construction_id});
+            });
         };
     });
