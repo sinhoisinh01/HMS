791f9adbbf59ba1a7e8c8f53a2775302428a253f
sinhoisinh01 <doanphucsinh@gmail.com>
2017-03-18 01:07:24 +0700

    Enable to Export Google Spreadsheet with Hard code, need get variable from request

---
+++ app/Http/Controllers/ExportGetDataController.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Subcategory;
+use App\Models\SubcategoryWork;
+use App\Models\Work;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Lumen\Routing\Controller;
+
+class ExportGetDataController extends Controller
+{
+	public function estimateTable( $categoryID ){
+    	$categoryFull = Subcategory::where('category_id', $categoryID)
+            ->select("subcategories.id","subcategories.name")
+            ->orderBy('subcategories.no')
+            ->with(['subcategoryWorks'=> function($q){
+                        $q->select("subcategory_work.subcategory_id", "subcategory_work.id", "works.code","works.name","works.unit");
+                        $q->orderBy('no');
+                        $q->join('works', 'subcategory_work.work_id', '=', 'works.id');
+                    },
+                    
+                    'subcategoryWorks.descriptions'=> function($q){
+                        $q->select("descriptions.subcategoryWork_id","descriptions.name","amount","length","width","height","value");
+                        $q->orderBy('no');
+                    }
+                    ])
+            ->get()->toArray();
+        $array = []; 
+        $subcategoriesLength = count($categoryFull); 
+        $no = 1;
+        for($i = 0; $i < $subcategoriesLength; $i++){
+            $subcategory = $categoryFull[$i];
+            unset($subcategory['id']);
+            unset($subcategory['subcategory_works']);// remove unecessary properties
+            array_unshift($subcategory,"*******");// add number order and empty element to array for empty columns
+            array_unshift($subcategory,$no);
+            $array[] = $subcategory;
+            $no++;
+            $worksLength = count($categoryFull[$i]["subcategory_works"]);
+            for($j = 0; $j < $worksLength; $j++){
+                $work = $categoryFull[$i]["subcategory_works"][$j];
+                unset($work["subcategory_id"]);
+                unset($work['id']);
+                unset($work['descriptions']);
+                array_unshift($work,$no);
+                $array[] = $work;
+                $no++;
+                $descriptionsLength = count($categoryFull[$i]["subcategory_works"][$j]["descriptions"]);
+                for($k = 0; $k < $descriptionsLength; $k++)
+                {
+                    $description = $categoryFull[$i]["subcategory_works"][$j]["descriptions"][$k];
+                    unset($description["subcategoryWork_id"]);
+                    array_unshift($description,"");
+                    array_unshift($description,$no);
+                    $description = array_slice($description,0,3,true) + array("unit"=>"") + array_slice($description,3,(count($description)-1),true);
+                    $array[] = $description;
+                    $no++;
+                }
+            }
+        }
+  		return $array;
+    }
+
+    // return values with Google Spreadsheet Format
+    public function getEstimateTableData( $categoryID )
+    {
+        $data = $this->estimateTable( $categoryID );
+        $values = [
+            [
+                'TT', 'Mã', 'Hạng mục/Công tác', 'Đơn vị', 'Số lượng', 'Dài', 
+                'Rộng', 'Cao', 'Khối lượng', 'Đơn giá', 'Thành tiền'
+            ]
+        ];
+        for ( $i = 0; $i < sizeof( $data ); $i++ ) {
+            $tmpArr = [];
+            
+            // Column A: STT
+            array_push( $tmpArr, ($i + 1) );
+
+            // Column B: if code exists, push code, else, push 1st element
+            ( isset( $data[$i]["code"] ) ) ?
+                array_push( $tmpArr, $data[$i]["code"] ) : array_push( $tmpArr, $data[$i][1] );
+
+            // Column C:
+            array_push( $tmpArr, $data[$i]["name"] );
+
+            // Column D: if unit exists, push unit, else, push null character
+            ( isset( $data[$i]["unit"] ) ) ?
+                array_push( $tmpArr, $data[$i]["unit"] ) : array_push( $tmpArr, '' );
+
+            // Column E: if amount exists, push unit, else, push null character
+            ( isset( $data[$i]["amount"] ) ) ?
+                array_push( $tmpArr, $data[$i]["amount"] ) : array_push( $tmpArr, '' );
+
+            // Column F: if amount exists, push unit, else, push null character
+            ( isset( $data[$i]["length"] ) ) ?
+                array_push( $tmpArr, $data[$i]["length"] ) : array_push( $tmpArr, '' );
+
+            // Column G: if width exists, push unit, else, push null character
+            ( isset( $data[$i]["width"] ) ) ?
+                array_push( $tmpArr, $data[$i]["width"] ) : array_push( $tmpArr, '' );
+
+            // Column H: if height exists, push unit, else, push null character
+            ( isset( $data[$i]["height"] ) ) ?
+                array_push( $tmpArr, $data[$i]["height"] ) : array_push( $tmpArr, '' );
+
+            // Column I: if value exists, push unit, else, push null character
+            ( isset( $data[$i]["value"] ) ) ?
+                array_push( $tmpArr, $data[$i]["value"] ) : array_push( $tmpArr, '' );
+
+            // Column J: if price exists, push unit, else, push null character
+            ( isset( $data[$i]["price"] ) ) ?
+                array_push( $tmpArr, $data[$i]["price"] ) : array_push( $tmpArr, '' );
+
+            // Column K: if row is work (has price), push formular to get total, else, push null character
+            ( isset( $data[$i]["price"] ) ) ?
+                array_push( "=I" . ( $i + 1 ) . "+J" . ( $i + 1 ) ) : array_push( $tmpArr, '' );
+
+            // push to values array
+            array_push( $values, $tmpArr );
+        }
+        return $values;
+    }
+
+    public function summaryTable( $categoryID )
+    {
+    	
+    }
+}
--- app/Http/Controllers/ExportGoogleSheetController.php
+++ app/Http/Controllers/ExportGoogleSheetController.php
@@ -7,10 +7,14 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Lumen\Routing\Controller;
+use App\Http\Controllers\ExportGetDataController;
 use Google_Client;
 use Google_Service_Sheets;
 use Google_Service_Sheets_Spreadsheet;
 use Google_Service_Sheets_SpreadsheetProperties;
+use Google_Service_Sheets_Request;
+use Google_Service_Sheets_BatchUpdateSpreadsheetRequest;
+use Google_Service_Sheets_ValueRange;
 
 class ExportGoogleSheetController extends Controller
 {
@@ -49,24 +53,75 @@ class ExportGoogleSheetController extends Controller
         $client->refreshToken(Auth::user()->refresh_token);
         $service = new Google_Service_Sheets($client);
 
-        $properties = new Google_Service_Sheets_SpreadsheetProperties();
+        // use exportGetDataController
+        $exportGetData = new ExportGetDataController();
+
+        /*$properties = new Google_Service_Sheets_SpreadsheetProperties();
         $properties->setTitle("Dự toán");
 
         $sheet = new Google_Service_Sheets_Spreadsheet();
         $sheet->setProperties($properties);
 
-        $response = $service->spreadsheets->create($sheet);
+        $response = $service->spreadsheets->create($sheet);*/
 
-        $spreadsheetId = $response->spreadsheetId;
+        //$spreadsheetId = $response->spreadsheetId;
+        $spreadsheetId = '1i-eEw77VhWq9X1ROcPdzmYUXC8noZyKFvpqsItnb3K4';
 
-        $requests[] = new Google_Service_Sheets_Request(array(
+        /*Set Data for Spreadsheet*/
+        /*$requests[] = new Google_Service_Sheets_Request(array(
 		  'updateCells' => array(
 		    "start" => array(
 	          "sheetId" => 0,
 	          "rowIndex" => 0,
 	          "columnIndex" => 0
 	        ),
-		  )
+	        "rows" => array(
+	          array(
+	            "values" => array(
+	              array(
+	                "userEnteredValue" => array("numberValue" => 1),
+	                "userEnteredFormat" => array(
+	                	"backgroundColor" => array("red" => 1)
+            		)
+	              ), 
+	              array(
+	                "userEnteredValue" => array("numberValue" => 2),
+	                "userEnteredFormat" => array(
+	                	"backgroundColor" => array("blue" => 1)
+            		)
+	              ), 
+	              array(
+	                "userEnteredValue" => array("numberValue" => 2),
+	                "userEnteredFormat" => array(
+	                	"backgroundColor" => array("green" => 1)
+            		)
+	              )
+	            )
+	          )
+	        ),
+	        "fields" => "userEnteredValue,userEnteredFormat.backgroundColor"
+	  	   )
 		));
+		
+
+		$batchUpdateRequest = new Google_Service_Sheets_BatchUpdateSpreadsheetRequest(array(
+		  'requests' => $requests
+		));
+
+		$response = $service->spreadsheets->batchUpdate($spreadsheetId,
+		    $batchUpdateRequest);*/
+		
+		$range = 'Sheet1!A1:K40';
+		$values = $exportGetData->getEstimateTableData(1);
+	  	$optParams = [];
+	  	$optParams['valueInputOption'] = 'USER_ENTERED';
+		$body = new Google_Service_Sheets_ValueRange( array(
+  			'values' => $values
+		) );
+		$result = $service->spreadsheets_values->update($spreadsheetId, $range, $body, $optParams);
+
+		
+		var_dump( $result );
+		//var_dump($test->estimateTable(1));
     }
 }
